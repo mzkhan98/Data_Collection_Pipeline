454923b957ebe32e10ee1aaf5273f18705ce649a
mzkhan98 <mohammadzainkhan1998@gmail.com>
2022-06-30 15:18:47 +0100

    readme

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time, datetime
 import pandas as pd
+import os
 import boto3
 import logging
 from botocore.exceptions import ClientError
